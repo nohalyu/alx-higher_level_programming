8381f1ad03388b08d8b555403268107f5a0b7931
Lyu <88846431+nohalyu@users.noreply.github.com>
2022-07-06 23:27:48 +0300

    Add files via upload

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains the read_file function
+"""
+
+
+def read_file(filename=""):
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            print(line, end='')
